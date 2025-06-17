664caae7b2f2053f494eee3123743ee7244a465e
luanarruda <luanamarrudaa@gmail.com>
2025-06-17 13:02:51 -0300

    Refactor: Consolidate alterarContexto logic

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ const longoBtn = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 
 focoBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'foco');
-  banner.setAttribute('src', 'imagens/foco.png');
+ alterarContexto('foco');
 });
 
 curtoBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto');
-  banner.setAttribute('src', 'imagens/descanso-curto.png');
+  alterarContexto('descanso-curto');
 });
 
 longoBtn.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-longo');
-  banner.setAttribute('src', 'imagens/descanso-longo.png');
+  alterarContexto('descanso-longo');
 });
+
+function alterarContexto(contexto) {
+	html.setAttribute('data-contexto', contexto);
+	banner.setAttribute('src', `imagens/${contexto}.png`);
+}
