3de47c22d9a39cf0b2fe1cd3518c7a4cbdf8c5d5
luanarruda <luanamarrudaa@gmail.com>
2025-06-18 13:49:02 -0300

    feat: add sound effects for start, pause, and timer end

--- script.js
+++ script.js
@@ -18,7 +18,9 @@ const startPauseBtn = document.querySelector('#start-pause');
 
 let intervaloId = null;
 
-
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 musicaFocoInput.addEventListener('change', () => {
 	if(musica.paused) {
@@ -69,8 +71,9 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
 	if(tempoDecorridoEmSegundos <= 0) {
-		zerar();
+		audioTempoFinalizado.play();
 		alert('Tempo esgotado!');
+		zerar();
 		return;
 	}
 	tempoDecorridoEmSegundos -= 1
@@ -81,9 +84,11 @@ startPauseBtn.addEventListener('click', iniciarOuPausar);
 
 function iniciarOuPausar() {
 	if(intervaloId) {
+		audioPausa.play();
 		zerar();
 		return;
 	}
+	audioPlay.play();
 	intervaloId = setInterval(contagemRegressiva, 1000);
 }
 
