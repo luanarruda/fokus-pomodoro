1320441a193c1383bef66c8c895296c38ed67e9c
luanarruda <luanamarrudaa@gmail.com>
2025-06-19 23:17:46 -0300

    feat: implement task form submission with localStorage

--- script-crud.js
+++ script-crud.js
@@ -2,9 +2,21 @@
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
-	//toggle = ""alternancia"" da classe 'hidden'
+	//toggle = ""alternancia"" da classe 'show'
 	formAdicionarTarefa.classList.toggle('show')
 
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+	evento.preventDefault();
+	const tarefa = {
+		descricao: textArea.value,
+	}
+	tarefas.push(tarefa)
+	localStorage.setItem('tarefas', tarefas)
 })
